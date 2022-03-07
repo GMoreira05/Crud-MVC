6e00349db4ccc061f17a4b1708a0722cb29adbc3
Gabriel <henrique_gabriel11@outlook.com>
2022-03-07 14:12:21 -0300

    CSS Adicionado

--- View/modules/Pessoa/CadastroPessoa.php
+++ View/modules/Pessoa/CadastroPessoa.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Pessoa</title>
+
+    <style>
+        input{
+            width: 500px;
+        }
+    </style>
 </head>
 <body>
     <form action="/pessoa/save">
