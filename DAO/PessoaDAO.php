ff6d9988bb548000fe0a2798d5abc3cbe976ac5c
= <=>
2022-03-14 08:40:55 -0300

    Banco de Dados

--- DAO/PessoaDAO.php
+++ DAO/PessoaDAO.php
@@ -2,12 +2,22 @@
 
     class PessoaDAO
     {
-        function __construct(){
+        private $conexao;
 
+        function __construct(){
+            $dns = "mysql:host=localhost;dbname=db_sistema";
+            $user = "root";
+            $pass = "etecjau";
+
+            $this->conexao = new PDO($dns, $user, $pass);
         }
 
         function insert(PessoaModel $model){
-            
+            $sql = "INSERT INTO pessoa (nome, rg, cpf, data_nascimento, email, telefone, endereco) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+            $stmt = $this->conexao->prepare($sql);
+            $stmt->bindValue(1, $model->nome);
+            $stmt->execute();
         }
     }
 
