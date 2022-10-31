c7fe8e5cd650bc2cebbe109beb47dfe638e0a00d
GMoreira05 <henrique_gabriel11@outlook.com>
2022-10-31 12:13:55 -0300

    Atualização na Lista

--- App/View/modules/Usuario/ListaUsuarios.php
+++ App/View/modules/Usuario/ListaUsuarios.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Produtos</title>
+    <title>Lista de Usuários</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <div class="container">
-        <h1>Lista de Produtos</h1>
+        <h1>Lista de Usuários</h1>
         <table class="bordered striped centered highlight responsive-table">
             <thead>
                 <tr>
